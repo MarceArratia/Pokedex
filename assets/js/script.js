7db89e0b394e81240570c20cfdce82a2e73a9420
MarceArratia <marratiaf@hotmail.com>
2020-07-08 23:45:43 -0400

    pokemon por tipo,grafico,ajax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 function buscador(){
   let pokeNombre = $('#inputPokemon').val();
   
-  console.log(pokeNombre);
+//  console.log(pokeNombre);
   $.ajax({
     url: 'https://pokeapi.co/api/v2/pokemon/'+pokeNombre,
     contentType: "application/json",
@@ -11,7 +11,7 @@ function buscador(){
         card(result);
     }
     ,error:function(result){
-      console.log(result);
+      $('#cardShow').css('display','none');
       $('#imgPokemon').attr("src","./assets/img/informacion.png");
   }
 })
@@ -26,7 +26,6 @@ function selectTipo(){
     success: function(result){
       $.each(result.results,function( name, url ){
         $("#selectPokemon").append('<option value='+url.url+'>'+url.name+'</option>');
-      
       })
        
     }
@@ -38,31 +37,103 @@ function selectTipo(){
 }
 
 function visualPokemon(){
-  $('#showPokemon').append('');
+  $('#showPokemon').empty();
+  $('#showPokemon').css('display','block');
+  $('#cardShow').css('display','none');
   let selectTipo= $('#selectPokemon option:selected').val();
-  $.ajax({
-    url: selectTipo,
-    contentType: "application/json",
-    dataType: 'json',
-    method:'GET',
-    success: function(result){
-      let table = '<table class="tablePokemon table table-striped table-dark"><th>Nombre</th><th>Descripción</th>';
-    $.each(result.pokemon,function( name, url ){
-  console.log(url.pokemon.name);
-  table+='<tr><td>'+url.pokemon.name+'</td><td><a href='+url.pokemon.url+'>Descripción</a></td></tr>'
-      
+  let pokemon=[];
+  var promise = $.ajax({
+    url : selectTipo,
+    method:'GET', 
+    success : function(result) {
+      $.each(result.pokemon,function( name, url ){
+        pokemon.push(url.pokemon.url);
       })
-      table+='</table>';
-      $('#showPokemon').append(table);
+      },
+    error : function(xhr,errmsg,err) {
+      console.log(xhr.status + ": " + xhr.responseText);
     }
-    ,error:function(result){
-      console.log(result);
-      $('#imgPokemon').attr("src","./assets/img/informacion.png");
-  }
-})
+  });
+
+  promise.then(function(){
+      pokemon.forEach(element => {
+        //console.log(element)
+       $.ajax({
+          url : element,
+          method:'GET', 
+          success : function(result) {
+            //console.log(result);
+             $('#showPokemon').append('<div class="card"><div class="card-body"><h3>'+result.name+'</h3><img src='+result.sprites.front_default+'></div></div>');
+          }
+          ,error : function(xhr,errmsg,err) {
+            console.log(xhr.status + ": " + xhr.responseText);
+          }
+        });
+      });
+  //
+ 
+  
+});
+
+
+
 }
 
 function card(result){
-  console.log(result);
+  $('#nameDescription').empty();
+  $('#cardShow').css('display','block');
+  $('#showPokemon').css('display','none');
+  //console.log(result);
+  let tipos = '<p class="card-text">types : ';
     $('#imgPokemon').attr("src",result.sprites.front_default);
+   $('#nameDescription').append('<h5 class="card-title">'+result.name+'</h5>');
+   result.types.forEach(element => {
+    
+     tipos+= '-'+ element.type.name+' ';
+   });
+   tipos+= '</p>'
+   $('#nameDescription').append(tipos);
+   let habilidad = 'Habilidades: <ul class="list-group list-group-flush">';
+   result.abilities.forEach(element => {
+     habilidad+=' <li class="list-group-item">'+element.ability.name+'</li>'
+   });
+   habilidad+= '</ul>';
+   $('#nameDescription').append(habilidad);
+   //Grafico
+   let data= [];
+   result.stats.forEach(element => {
+     data.push({label:element.stat.name,y:element.base_stat});
+     //console.log(element.base_state);
+   });
+   console.log(data);
+   var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    title: {
+      text: "Estadísticas"
+    },
+    axisX: {
+      interval: 1
+    },
+    axisY: {
+      title: "States",
+      scaleBreaks: {
+        type: "wavy",
+        customBreaks: [{
+          startValue: 10,
+          endValue: 110
+          },
+          {
+            startValue: 30,
+            endValue: 150
+          }
+      ]}
+    },
+    data: [{
+      type: "bar",
+     // toolTipContent: "<img src=\"https://canvasjs.com/wp-content/uploads/images/gallery/javascript-column-bar-charts/\"{url}\"\" style=\"width:40px; height:20px;\"> <b>{label}</b><br>Budget: ${y}bn<br>{gdp}% of GDP",
+      dataPoints:data
+    }]
+  });
+  chart.render();
+  
 }
